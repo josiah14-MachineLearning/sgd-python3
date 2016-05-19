abfb161d50a4186d4fdd9c8e16e45ba2928bf2ab
Josiah Berkebile <jgberkebile@dstsystems.com>
2016-05-19 13:20:30 -0500

    add a rough draft for a nix shell instantiated dev environment

---
+++ default.nix
@@ -0,0 +1,19 @@
+# This is non-functional at the moment.  Needs to be tweaked to
+# work on OSX and Ubuntu.  I have no NixOS install to test
+# against.
+with import <nixpkgs> {}; {
+  python3Env = stdenv.mkDerivation rec {
+    name = "python3-env";
+    version = "0.1";
+    src = ./.;
+    buildInputs = [
+      git
+      libxml2
+      libxslt
+      libzip
+      zlib
+      stdenv
+      python3-3.5.0
+    ]
+  }
+}
