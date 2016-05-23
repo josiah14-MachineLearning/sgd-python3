e8e7a494ae447ebf173a8828c25306318358a552
Josiah Berkebile <jgberkebile@dstsystems.com>
2016-05-23 17:12:56 -0500

    A somewhat functional SGD implementation completed

--- sgd.py
+++ sgd.py
@@ -1,28 +1,21 @@
 import numpy as np
-from math import sqrt
+from math import sqrt, fabs
+from functools import reduce
 
 num_features = 100
 known_ratings_vec = np.random.randint(0, 6, size=1000)
 mean_rating = np.mean(known_ratings_vec)
 print("Mean: " + str(mean_rating))
 
-def initialize_feature_vectors(numFeatures, knownRatings):
+def initialize_feature_vectors(numFeatures, meanRating):
     qi = np.random.random_sample(numFeatures)
     pu = np.random.random_sample(numFeatures)
-
     # shift the initial dot product close to the mean
-    shift_factor = sqrt(mean_rating / np.dot(qi, pu))
-    qi = shift_factor * qi
-    pu = shift_factor * pu
-
-    print([qi, pu])
-
-    return [qi, pu]
+    shift_factor = sqrt(meanRating / np.dot(qi, pu))
+    return [shift_factor * qi, shift_factor * pu]
 
 def calc_error(qi, pu, knownRating):
     error = knownRating - np.dot(qi, pu)
-    # print("Error: " + str(error))
-    # print("Dot: " + str(np.dot(qi, pu)))
     return error
 
 def calc_new_qi(qi, pu, error, learningRate, lambdaConst):
@@ -31,18 +24,21 @@ def calc_new_qi(qi, pu, error, learningRate, lambdaConst):
 def calc_new_pu(qi, pu, error, learningRate, lambdaConst):
     return pu + learningRate * (error * qi - lambdaConst * pu)
 
-def update_feature_vectors(qi, pu, knownRating, learningRate, lambdaConst):
-    new_qi = calc_new_qi(qi, pu, calc_error(qi, pu, knownRating), learningRate, lambdaConst)
-    new_pu = calc_new_pu(new_qi, pu, calc_error(new_qi, pu, knownRating), learningRate, lambdaConst)
-    return [new_qi, new_pu, calc_error(new_qi, new_pu, knownRating)]
+def update_feature_vectors(qi, pu, knownRating, calcError, learningRate, lambdaConst):
+    error = calcError(qi, pu, knownRating)
+    new_qi = calc_new_qi(qi, pu, error, learningRate, lambdaConst)
+    new_pu = calc_new_pu(qi, pu, error, learningRate, lambdaConst)
+    return [new_qi, new_pu, calcError(new_qi, new_pu, knownRating)]
 
-def sgd(knownRating, qi, pu, learningRate, lambdaConst, numIterations):
-    new_qi, new_pu = [qi[:], pu[:]]
+def sgd(knownRating, learningRate, lambdaConst, numIterations):
+    qi, pu = initialize_feature_vectors(num_features, mean_rating)
     for i in range(numIterations):
-        new_qi, new_pu, error = update_feature_vectors(new_qi, new_pu, knownRating, learningRate, lambdaConst)
-    return [new_qi, new_pu, error]
+        qi, pu, error = update_feature_vectors(qi, pu, knownRating, calc_error, learningRate, lambdaConst)
+        if fabs(error) < 0.001:
+            break
+    return [qi, pu, error, knownRating]
 
-qi, pu = initialize_feature_vectors(num_features, known_ratings_vec)
-guessed_ratings_with_error = [ sgd(knownRating, qi, pu, 0.08, 20, 100) for knownRating in known_ratings_vec ]
+guessed_ratings_with_error = [ sgd(knownRating, 0.27, 0.023, 5) for knownRating in known_ratings_vec ]
 
-[ print(result) for result in guessed_ratings_with_error ]
+[ print([fabs(result[2]), np.dot(result[0], result[1]), result[3]]) for result in guessed_ratings_with_error ]
+print("Average Error: " + str(reduce(lambda x, y: x + y, map(lambda result: fabs(result[2]), guessed_ratings_with_error)) / len(guessed_ratings_with_error)))
